bfda3599b7968de8f990ba91cbd549c5e07f1595
lucasemanoel3103 <160548558+lucasemanoel3103@users.noreply.github.com>
2025-02-12 20:44:39 -0300

    Criação do server http, implementação do controller e services

---
+++ src/controllers/podcasts-controller.ts
@@ -0,0 +1,13 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { serviceListEpisodes } from '../services/list-episodes-service'
+
+export const getListEpisodes = async (
+  req: IncomingMessage,
+  res: ServerResponse
+) => {
+
+const content = await serviceListEpisodes();
+
+  res.writeHead(200, { "contents-type": "application/json" });
+  res.end(JSON.stringify(content));
+};
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import * as http from "http";
+import {getListEpisodes} from './controllers/podcasts-controller'
+
+const server = http.createServer(
+  async (req: http.IncomingMessage, res: http.ServerResponse) => {
+
+    if(req.method === "GET") {
+        await getListEpisodes(req, res); 
+    }
+  }
+);
+
+const port = process.env.PORT
+
+server.listen(port, () => {
+  console.log(`Servidor iniciado na porta ${port}`);
+});
