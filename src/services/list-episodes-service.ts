6172936d0af6b0f42ac74ca3c695663cac786f6c
lucasemanoel3103 <160548558+lucasemanoel3103@users.noreply.github.com>
2025-02-13 19:56:39 -0300

    Implementação de models e repository

--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,18 +1,7 @@
+import { repositoryPodcast } from "../repositories/podcasts-repository";
+
 export const serviceListEpisodes = async () => {
-  const data = [
-    {
-      podcastName: "flow",
-      episode: "CBUM - Flow #319",
-      videoId: "pQSumUfS30",
-      categories: ["saúde", "esporte", "bodybuilder"],
-    },
-    {
-      podcastName: "flow",
-      episode: "RUBENS BARRICHELLO - Flow #339",
-      videoId: "4KDGTdi0V4I",
-      categories: ["corrida", "esporte"],
-    },
-  ];
+  const data = await repositoryPodcast;
 
   return data;
 };
