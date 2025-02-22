4b3d90f043a445aac0667f7dcf69764d543949d6
lucasemanoel3103 <160548558+lucasemanoel3103@users.noreply.github.com>
2025-02-22 14:30:32 -0300

    Implementação das rotas de list e filter

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,5 +1,6 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { serviceListEpisodes } from '../services/list-episodes-service'
+import { serviceFilterEpisodes } from "../services/filter-episodes-services";
 
 export const getListEpisodes = async (
   req: IncomingMessage,
@@ -7,6 +8,16 @@ export const getListEpisodes = async (
 ) => {
 
 const content = await serviceListEpisodes();
+
+  res.writeHead(200, { "contents-type": "application/json" });
+  res.end(JSON.stringify(content));
+};
+
+export const getFilterEpisodes = async (
+  req: IncomingMessage,
+  res: ServerResponse
+) => {
+  const content = await serviceFilterEpisodes('flow');
 
   res.writeHead(200, { "contents-type": "application/json" });
   res.end(JSON.stringify(content));
